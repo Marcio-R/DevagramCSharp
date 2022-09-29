cccf080b79ed29ca03165b06e9003de9c3de52be
marcio ronaldo alves <marcinho-ronald@hotmail.com>
2022-09-29 08:16:38 -0300

    Criado a API de comentario.

--- DevagramCSharp/Controllers/ComentarioController.cs
+++ DevagramCSharp/Controllers/ComentarioController.cs
@@ -1,4 +1,5 @@
-﻿using DevagramCSharp.Models;
+﻿using DevagramCSharp.Dtos;
+using DevagramCSharp.Models;
 using DevagramCSharp.Repository;
 using DevagramCSharp.Repository.Implementation;
 using Microsoft.AspNetCore.Http;
@@ -13,16 +14,43 @@ namespace DevagramCSharp.Controllers
         private readonly ILogger<ComentarioController> _logger;
         private readonly IComentarioRepository _comentarioRepository;
 
-        public ComentarioController(ILogger<ComentarioController> logger,IUsuarioRepository usuarioRepository, IComentarioRepository comentarioRepository) : base(usuarioRepository)
+        public ComentarioController(ILogger<ComentarioController> logger, IUsuarioRepository usuarioRepository, IComentarioRepository comentarioRepository) : base(usuarioRepository)
         {
             _logger = logger;
             _comentarioRepository = comentarioRepository;
         }
 
         [HttpPut]
-        public IActionResult Comentar([FromBody] ComentarioDto comentariodto)
+        public IActionResult Comentar([FromBody] ComentarioRequisicaoDto comentariodto)
         {
+            try
+            {
+                if (comentariodto != null)
+                {
+                    if (String.IsNullOrEmpty(comentariodto.Descricao) || String.IsNullOrWhiteSpace(comentariodto.Descricao))
+                    {
+                        _logger.LogError("O comentario rescebido estava vazio");
+                        return BadRequest("Por favor coloque seu comentario");
+                    }
+                    Comentario comentario = new Comentario();
+                    comentario.Descricao = comentariodto.Descricao;
+                    comentario.IdPublicacao = comentariodto.IdPublicacao;
+                    comentario.IdUsuario = LerToken().Id;
 
+                    _comentarioRepository.Comentar(comentario);
+                }
+                return Ok("Comentario salvo com sucesso!");
+            }
+            catch (Exception e)
+            {
+
+                _logger.LogError("Ocorreu um erro ao comentar");
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErroRespostasDto()
+                {
+                    Descricao = $"Ocorreu um erro ao comentar {e.Message}",
+                    Status = StatusCodes.Status500InternalServerError
+                });
+            }
         }
     }
 }
---
+++ DevagramCSharp/Dtos/ComentarioRequisicaoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DevagramCSharp.Dtos
+{
+    public class ComentarioRequisicaoDto
+    {
+        public int IdPublicacao { get; set; }
+        public string Descricao { get; set; }
+    
+    }
+}
